3ee57281ed42da3c1520c33aedbf76dff2b0c63d
shreyasingh272004 <singhshreya272004@gmail.com>
2025-07-02 05:29:03 +0000

    datatypes.js

---
+++ BASICS/datatypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all JS code as newer version
+
+//alert("hello")
+//alert(3+3) error bcz we're using nodejs not browser
+
+console.log(3+3)
+console.log("Shreya")
+
+
+let name="shreya"
+let age=0
+let state=null
+let isLoggedIn=false
+/*
+DATA TYPES-
+            int number=2 to power 52
+            bigint
+            string=""
+            boolean= true/false
+            nullstandalone value
+            undefined
+            symbol=unique
+            object
+ */
+
+console.log(typeof name);
---
+++ BASICS/variables.js
@@ -0,0 +1,17 @@
+const account_Id=144553//const value will be fixed throught
+let accountEmail="singhshreya272004"//let is used majorly
+var accountPassword="12345"
+accountCity="Varanasi"
+//prefer do not use var bcz of issue in block scope nd functional scope
+//account_Id=2 this value will not be updated
+
+console.log(account_Id);
+accountEmail="abcd"
+accountPassword="3287"
+accountCity="Delhi"
+
+console.table([account_Id,accountEmail,accountPassword,accountCity])
+
+{
+    
+}
